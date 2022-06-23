360ad9b60f70a9ea07e44dabcb426c3b2f9627b4
Prashobh <81906847+prashobh4657@users.noreply.github.com>
2022-06-23 08:36:06 +0530

    Add files via upload

---
+++ codechef_practice.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define mod 1000000007
+#define endl "\n";
+#define fio ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t > 0)
+    {
+        t--;
+    }
+    return 0;
+}
