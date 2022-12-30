b0bf633b36a2edd9986e8fd040a5fd4af12a53d6
Prashobh <81906847+prashobh4657@users.noreply.github.com>
2022-12-30 11:42:06 +0530

    Add files via upload

---
+++ Word_break.cpp
@@ -0,0 +1,18 @@
+// *************************************************WORD BREAK***************************************************
+// Word Break 1 :
+//==> GFG link : https://practice.geeksforgeeks.org/problems/word-break1352/1
+//==> Leetcode link : https://leetcode.com/problems/word-break/description/
+
+// Word Break 2 :
+//==> GFG link : https://practice.geeksforgeeks.org/problems/word-break-part-23249/1#
+//==> Leetcode link : https://leetcode.com/problems/word-break-ii/description/
+
+// Till now no submissions made on leetcode;
+/*
+Word-break 2 ==> we need to print sentences hence backtracking : just see latest submission gfg.
+ (don't know why dp tag given in word-break 1);
+Word-break 1 ==> just solved using word-break 2 solution (backtracking) (but that is v. non-optimised)
+             ==> learn dp solution ; and implement here;
+*/
+// Word break 2 learnt from : pepcoding playlist video 16
+// word break 1 ==> try to learn from dp level 2 playlist (pepcoding) but it's code is not working till now.....
