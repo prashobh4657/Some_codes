bebf9b2b138532cafd6b52b19aacae1d88a8a337
Prashobh <81906847+prashobh4657@users.noreply.github.com>
2025-07-16 09:35:44 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,17 +2,32 @@
 using namespace std;
 #define int long long int
 #define mod 1000000007
-#define endl "\n";
-#define fio ios_base::sync_with_stdio(false); cin.tie(NULL);
-
+#define endl "\n"
+#define fio                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+template < typename T >
+    istream & operator >> (istream & input, vector < T > & v)
+    {
+        for (auto & i: v)
+            cin >> i;
+        return input;
+    }
+template < typename T >
+    ostream & operator << (ostream & output, vector < T > & v)
+    {
+        for (auto & i: v)
+            cout << i << " ";
+        return output;
+    }
 int32_t main()
 {
     fio;
     int t;
     cin >> t;
-    while (t > 0)
+    while (t--)
     {
-        t--;
+
     }
     return 0;
 }
